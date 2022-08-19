702f57087f7ceaa1fe065f4884a0e60480d45596
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-19 17:47:40 +0400

    some message

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int newNum = (((number%10) * 10)  + (number / 10 % 10)) * 10 + number / 100;
+        System.out.println(newNum);
     }
 }
